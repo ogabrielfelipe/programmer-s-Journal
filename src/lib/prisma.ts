4cf06333a301fe7b53d356ebce1e627fc0eb77d5
Gabriel Felipe <ogabriel.v.felipe@gmail.com>
2023-03-26 19:47:38 -0300

    adjusts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,63 +2,64 @@ import { PrismaClient } from '@prisma/client';
 import { hash } from 'bcryptjs';
 
 const prisma = new PrismaClient();
-/*
-async function connectDB(){
-    console.log("💥 Connecting to database...");
 
+type User = {
+    id: string,
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    role: string,
+    created_at: string,
+    updated_at: string
+}
+
+async function connectDB() {
     prisma.$connect()
-        .then(() => {        
-            console.log("✅ Connected to database");
-        })
-        .catch(err => {
-            console.error("👾 Failed to connect to the database.\n", err)
-            process.abort();
-
-        })
 }
 
 async function main() {
-    
+
     const FIRSTNAME = "Usuário"
-    const LASTNAME =  "Administrador"
+    const LASTNAME = "Administrador"
     const EMAIL = "admin@admin.com"
     const PASSWORD = "admin"
 
     connectDB()
-        .then(async () =>{
+        .then(async (resp) => {
             await prisma.user.findFirst({
-                where:{
+                where: {
                     email: EMAIL
                 }
-            }).then(async resp =>{
-                if (!resp){
-                    const passwordHash = await hash(PASSWORD, 8)
-        
-                    await prisma.user.create({
-                        data:{
-                            firstName: FIRSTNAME,
-                            lastName: LASTNAME,
-                            email: EMAIL,
-                            password: passwordHash
-                        },
-                        select:{
-                            id: true,
-                            email: true
-                        }
-                    })
-                }
-            })  
+            }).then(async (resp)  => {
+                    if (!resp){
+                        const passwordHash = await hash(PASSWORD, 8)
+
+                        await prisma.user.create({
+                            data: {
+                                firstName: FIRSTNAME,
+                                lastName: LASTNAME,
+                                email: EMAIL,
+                                password: passwordHash
+                            },
+                            select: {
+                                id: true,
+                                email: true
+                            }
+                        })
+                    }                
+            })
         })
 }
-  
+
 main()
-.then(async () => { 
-    await prisma.$disconnect()
-})
-.catch(async (e) => {
-    await prisma.$disconnect()
-    process.exitCode = 1;
-})
-*/
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        await prisma.$disconnect()
+        process.exitCode = 1;
+    })
+
 
 export default prisma;
